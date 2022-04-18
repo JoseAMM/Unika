a76e46c630c038d2f1916634a70fd4f0a3adaf88
José Antonio Medina Martinez <joseamm@comunidad.unam.mx>
2022-04-18 16:23:28 -0500

    cambio al database.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -12,14 +12,3 @@ function conectarDB(){
 }
 }
 ?>
-<!-- function conectarDB(){
-    $db = mysqli_connect('localhost', 'unikacor_user', 'Mercurial123.', 'unikacor_bienes_raices');
-
-    if(!$db) {
-
-    exit;
-} else {
-    
-    return $db;
-}
-} -->
